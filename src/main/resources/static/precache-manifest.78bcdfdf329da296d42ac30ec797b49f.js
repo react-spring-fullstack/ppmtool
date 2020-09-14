4499f1b2fb59828e4afd7b4f7e85481c25876611
Abrahan Romero <abrahan.romero.alvarez@gmail.com>
2020-09-13 21:28:00 -0500

    Add frontend layer.

---
+++ src/main/resources/static/precache-manifest.78bcdfdf329da296d42ac30ec797b49f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edce955c399138b03ace961ebb234ffb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "814acf2dd416d4756d11",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "449743d4366cffabacb3",
+    "url": "/static/css/main.10efe6f2.chunk.css"
+  },
+  {
+    "revision": "814acf2dd416d4756d11",
+    "url": "/static/js/2.f40514ee.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f40514ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449743d4366cffabacb3",
+    "url": "/static/js/main.f970990e.chunk.js"
+  },
+  {
+    "revision": "f0395242fcfe40790f57",
+    "url": "/static/js/runtime-main.85b40700.js"
+  }
+]);
